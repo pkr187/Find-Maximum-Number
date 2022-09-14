a17e103f6f0449a462a448a2728e4a6b4dcdb678
pkr187 <pankajroman187@gmail.com>
2022-09-14 23:33:27 +0530

    [pankaj] Add:Maximum-Integer-Number

---
+++ FindMaximumNumber/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace FindMaximumNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Find Maximum Problem!");
+
+        }
+    }
+}
---
+++ FindMaximumNumberTest/UnitTest.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+namespace FindMaximumNumberTest;
+
+namespace FindMaximumNumberTest
+{
+    //A - Arrange
+    //A - Act
+    //A - Assert
+    public class Tests
+    {
+        //TC 1.1
+        [Test]
+        public void GivenMaxFirstNumber_WhenAnalized_ShouldReturnMaxNumber()
+        {
+            FindMaximum max = new FindMaximum();
+            int num = max.FindMaxIntNumber(3, 1, 2);
+            Assert.AreEqual(num, 3);
+        }
+        //TC 1.2
+        [Test]
+        public void GivenMaxSecondNumber_WhenAnalized_ShouldReturnMaxNumber()
+        {
+            FindMaximum max = new FindMaximum();
+            int num = max.FindMaxIntNumber(1, 3, 2);
+            Assert.AreEqual(num, 3);
+        }
+        //TC 1.3
+        [Test]
+        public void GivenMaxThirdNumber_WhenAnalized_ShouldReturnMaxNumber()
+        {
+            FindMaximum max = new FindMaximum();
+            int num = max.FindMaxIntNumber(1, 2, 3);
+            Assert.AreEqual(num, 3);
+        }
+    }
+}
